38cb0bacfb3d72ad07532f4ca2c4ca1aabad7b63
An1mus <an1mus@icloud.com>
2022-12-03 18:42:12 +0200

    Chat window

--- client/src/Chat/index.tsx
+++ client/src/Chat/index.tsx
@@ -2,17 +2,27 @@ import React from 'react';
 import styles from './Chat.module.scss';
 
 const Chat: React.FC = () => {
-    return <div className={styles.container}>
-        Chat
-        <div className={styles.message}>
-            <div className={styles.author}>
-                author
+    return <main className={styles.container}>
+        <section className={styles.chatBox}>
+            <div className={styles.message}>
+                <div className={styles.author}>
+                    author
+                </div>
+                <div className={styles.text}>
+                    text
+                </div>
             </div>
-            <div className={styles.text}>
-                text
+
+            <div className={`${styles.message} ${styles.byAuthor}`}>
+                <div className={styles.author}>
+                    author
+                </div>
+                <div className={styles.text}>
+                    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+                </div>
             </div>
-        </div>
-    </div>;
+        </section>
+    </main>;
 }
 
 export default Chat;
