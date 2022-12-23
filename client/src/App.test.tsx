304d810f6504332c72b36e7c801997c7168f1d06
An1mus <an1mus@icloud.com>
2022-12-23 19:54:16 +0200

    Test fixes

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders the app', () => {
+test('Renders the app', async () => {
   // this is a workaround for issue #1695 in jsdom
   window.HTMLElement.prototype.scrollIntoView = function() {};
 
-  const {container} = render(<App />);
+  const {container} = await render(<App />);
   expect(container.querySelector('h1')).toHaveTextContent('Welcome to ChitChat')
 });
