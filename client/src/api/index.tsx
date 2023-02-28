67c9f988c80d057c8352a75b1893c90e805d50b5
An1mus <an1mus@icloud.com>
2023-02-28 14:22:12 +0200

    Prettier for the client

--- client/src/api/index.tsx
+++ client/src/api/index.tsx
@@ -1,14 +1,19 @@
 import io from 'socket.io-client';
-import uuid from "react-uuid";
+import uuid from 'react-uuid';
 
 const API_URL = `https://websocketchattest.herokuapp.com/`;
 
 export const socket = io(API_URL);
 
-export const sendMessage = (message: string, nickname: string) =>  {
-    socket.emit('message', { message, author: nickname, uuid: uuid(), date: new Date() });
-}
+export const sendMessage = (message: string, nickname: string) => {
+	socket.emit('message', {
+		message,
+		author: nickname,
+		uuid: uuid(),
+		date: new Date(),
+	});
+};
 
 export const connectWithNickName = (nickname: string) => {
-    socket.emit('userConnected', nickname)
+	socket.emit('userConnected', nickname);
 };
